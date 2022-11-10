884a4487773a9d1e41cf0ccd1dca6118668e9169
Massimo Girondi <massimo@girondi.net>
2022-11-10 22:50:34 +0100

    Add git username for messages

--- config.sh
+++ config.sh
@@ -1,6 +1,13 @@
 export HEDGEDOC_SERVER='https://hedgedoc.whatever.org'
 export HEDGEDOC_COOKIES_FILE='./key.conf'
 export GIT_REMOTE='git@whatever:user/repo'
+
+
+# These are used in the commit messages
+export GIT_USER='Hedgedoc backupper'
+export GIT_EMAIL='hedgedoc_backupper@local'
+
 export DO_GIT_PUSH=1
+
 
 export H='./cli/bin/hedgedoc'
--- do.sh
+++ do.sh
@@ -8,6 +8,8 @@ if ! [ -d notes ]; then
         mkdir notes
         git -C notes init
         git -C notes remote add origin $GIT_REMOTE
+        git -C notes config user.email "$GIT_EMAIL"
+        git -C notes config user.name "$GIT_USER"
         echo "archive.zip" >> notes/.gitignore
         git -C notes add .gitignore
 fi
